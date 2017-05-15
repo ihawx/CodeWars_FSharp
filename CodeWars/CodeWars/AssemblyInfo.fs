a231dfaff00cabbeb6e45818e4f1523a421b0487
Lubos <Lubos@DESKTOP-H1795JA>
2017-05-15 22:02:33 +0200

    [8 kyu] String repeat

---
+++ CodeWars/CodeWars/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace CodeWars.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("CodeWars")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("CodeWars")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("d8850c18-8eb5-4b49-8cdc-78b643db515f")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ CodeWars/CodeWars/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+
+//[8 kyu] String repeat : Write a function called repeatStr which repeats the given string string exactly n times.
+let repeatStr n s = 
+    [1..n]
+    |> List.map(fun x -> s)
+    |> String.Concat
+
+Console.ReadKey|>ignore
